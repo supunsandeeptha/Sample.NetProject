35517650e824e2fce5b69178196e126d42e54039
supunsandeeptha <sandeeptha.supun@gmail.com>
2019-05-30 14:19:43 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = " Supun Sandeeptha";
+
+            Int32 value32 = 16;
+            Int16 value16 = 16;
+            Int64 value64 = 64;
+
+            bool status = true;
+
+            for(int i =0; i < 10; i++)
+            {
+
+                Console.WriteLine("number is " + i);
+
+            }
+            //printing text on the screen
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My age is " + value32);
+
+            Console.Write("");
+
+            //reading input from the user
+            Console.ReadLine();
+
+           
+
+            //drawing a shape 
+
+
+
+        }
+    }
+}
